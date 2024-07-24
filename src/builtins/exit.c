b2878a7709830a1ff0a56ac785099bbf9ffa4c7b
Gautier Schwander <gautier.schwander@gmail.com>
2024-07-24 10:36:16 +0000

    add error for exit if there is more than 1 arg

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: gschwand <gschwand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 09:04:41 by gschwand          #+#    #+#             */
-/*   Updated: 2024/07/24 10:31:43 by gschwand         ###   ########.fr       */
+/*   Updated: 2024/07/24 10:35:14 by gschwand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,14 @@ void ft_exit(char **tab, t_env *env)
     unsigned char status;
     
     env_clear(&env);
+    if (tab[1] && tab[2])
+    {
+        printf("exit\n");
+        printf("minishell: exit: too many arguments\n");
+        free_tab(tab);
+        exit(1);
+    }
+    else
     if (tab[1])
     {
         status = (unsigned char) ft_atoi(tab[1]);
