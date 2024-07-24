8bdfcc0728d250eb4b5ed6f3785df2f242af687c
Gautier Schwander <gautier.schwander@gmail.com>
2024-07-24 10:33:30 +0000

    add unsigned char for exit

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: gschwand <gschwand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 09:04:41 by gschwand          #+#    #+#             */
-/*   Updated: 2024/07/24 09:11:14 by gschwand         ###   ########.fr       */
+/*   Updated: 2024/07/24 10:31:43 by gschwand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,12 +28,12 @@ void free_tab(char **tab)
 // surement ici que l'on va devoir gerer toute la memoire
 void ft_exit(char **tab, t_env *env)
 {
-    int status;
+    unsigned char status;
     
     env_clear(&env);
     if (tab[1])
     {
-        status = ft_atoi(tab[1]);
+        status = (unsigned char) ft_atoi(tab[1]);
         free_tab(tab);
         exit(status);
     }
