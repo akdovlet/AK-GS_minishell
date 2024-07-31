7e6362dc57802e274a80e010a9d86e382148573f
Gautier Schwander <gautier.schwander@gmail.com>
2024-07-31 18:05:21 +0200

    1

--- src/expend/wildcard.c
+++ src/expend/wildcard.c
@@ -6,14 +6,16 @@
 /*   By: gschwand <gschwand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 16:11:27 by gschwand          #+#    #+#             */
-/*   Updated: 2024/07/31 17:33:39 by gschwand         ###   ########.fr       */
+/*   Updated: 2024/07/31 18:03:02 by gschwand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "wildcard.h"
+#include "expend.h"
 
 // exec de "ls" et recup de la liste dans un dossier tmp
 // fonctionne comme l'expenion de variable 
 // Sortie d'une string avec tous les files demande dans e dossier courant
+
+
 
 char *wildcard()
