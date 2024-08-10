1ddb4ac3f17ac8498247f8e5b5aebfadb883eeb3
Gautier Schwander <gautier.schwander@gmail.com>
2024-08-10 14:48:59 +0000

    bug with char

--- src/expand/wildcard.c
+++ src/expand/wildcard.c
@@ -6,7 +6,7 @@
 /*   By: gschwand <gschwand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 16:11:27 by gschwand          #+#    #+#             */
-/*   Updated: 2024/08/05 17:05:16 by gschwand         ###   ########.fr       */
+/*   Updated: 2024/08/10 14:44:58 by gschwand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,17 +75,163 @@ t_files *ft_recover_files(void)
     return (files);
 }
 
+int ft_strcmp_start_end_wildcard(char *file, char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i] && file[i])
+    {
+        if (str[i] == '*')
+            return (true);
+        if (str[i] != file[i])
+            return (false);
+        i++;
+    }
+    return (true);
+}
+
+void ft_lstdelone_files(t_files **files, t_files *to_delete)
+{
+    t_files *tmp;
+
+    if (*files == to_delete)
+    {
+        *files = (*files)->next;
+        free(to_delete);
+        return;
+    }
+    tmp = *files;
+    while (tmp && tmp->next != to_delete)
+        tmp = tmp->next;
+    if (tmp && tmp->next == to_delete)
+    {
+        tmp->next = to_delete->next;
+        free(to_delete);
+    }
+}
+
+int ft_strcmp_end_start_wildcard(char *file, char *str)
+{
+    int i;
+    int j;
+    int k;
+
+    if (!str || !file)
+        return (printf("str or file is NULL\n"), false);
+    i = ft_strlen(file) - 1;
+    j = ft_strlen(str) - 1;
+    k = 0;
+    while (i - k >= 0 && j - k >= 0)
+    {
+        if (str[j - k] == '*')
+            return (true);
+        if (str[j - k] != file[i - k])
+            return (false);
+        k++;
+    }
+    return (true);
+}
+
+void ft_lstcomp_wildcard(t_files **files, char *str, int (*strcmp)(char*, char*))
+{
+    t_files *tmp;
+    t_files *tmp2;
+
+    tmp = *files;
+    while (tmp)
+    {
+        if (!strcmp(tmp->name, str))
+        {
+            tmp2 = tmp;
+            tmp = tmp->next;
+            ft_lstdelone_files(files, tmp2);
+        }
+        else
+            tmp = tmp->next;
+    }
+}
+
+char *write_files(t_files *files)
+{
+    t_files *tmp;
+    char *res;
+    int lenres;
+
+    tmp = files;
+    lenres = 0;
+    while (tmp)
+    {
+        lenres += ft_strlen(tmp->name) + 1;
+        printf("lenres = %d\n", lenres);
+        printf("tmp->name = %s\n", tmp->name);
+        tmp = tmp->next;
+    }
+    res = ft_calloc(sizeof(char), lenres + 1);
+    if (!res)
+        return (perror("malloc"), NULL);
+    tmp = files;
+    while (tmp)
+    {
+        printf("tmp->name = %s\n", tmp->name);
+        ft_strlcat(res, tmp->name, lenres + 1);
+        ft_strlcat(res, " ", lenres + 1);
+        tmp = tmp->next;
+    }
+    return (res);
+}
+
+void ft_free_lst_files(t_files *files)
+{
+    t_files *tmp;
+
+    while (files)
+    {
+        tmp = files;
+        files = files->next;
+        free(tmp);
+    }
+}
+
+char *sort_files(t_files *files, char *str)
+{
+    char *res;
+
+    ft_lstcomp_wildcard(&files, str, ft_strcmp_start_end_wildcard);
+    ft_lstcomp_wildcard(&files, str, ft_strcmp_end_start_wildcard);
+    ft_print_lst_files(files);
+    res = write_files(files);
+    ft_free_lst_files(files);
+    return (res);
+}
+
+void ft_supp_point_files(t_files **files)
+{
+    t_files *tmp;
+    
+    tmp = *files;
+    *files = (*files) -> next;
+    free(tmp);
+    tmp = *files;
+    *files = (*files) -> next;
+    free(tmp);
+}
+
+
+
 // res will return all the files who correspond to the wildcard
 char  *expand_wildcard(char *str)
 {
     t_files *files;
     char *res;
     
-    (void)str;
     res = NULL;
     files = ft_recover_files();
-    // res = sort_files(files, str);
-    ft_print_lst_files(files);
+    if (!files)
+        return (NULL);
+    ft_supp_point_files(&files);
+    res = sort_files(files, str);
+    printf("res = %s\n", res);
     return (res);
 }
 
