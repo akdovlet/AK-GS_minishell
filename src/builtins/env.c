ed17d3f7edbf76a934038f04bd83de4fc9c0b77c
Gautier Schwander <gautier.schwander@gmail.com>
2024-09-26 15:16:35 +0200

    correction exit

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: gschwand <gschwand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 14:18:24 by gschwand          #+#    #+#             */
-/*   Updated: 2024/09/23 14:12:34 by gschwand         ###   ########.fr       */
+/*   Updated: 2024/09/26 14:46:48 by gschwand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	ft_env(char **argv, t_env *env)
 {
 	if (argv[1])
-		ft_dprintf(STDERR_FILENO, "env: too many arguments\n");
+		ft_dprintf(STDERR_FILENO, "minishell: env: too many arguments\n");
 	else
 	{
 		while (env)
--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: gschwand <gschwand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 09:04:41 by gschwand          #+#    #+#             */
-/*   Updated: 2024/09/25 18:11:30 by gschwand         ###   ########.fr       */
+/*   Updated: 2024/09/26 15:15:52 by gschwand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,36 +20,53 @@ static int	check_only_digit(char *str)
 	while (str[i])
 	{
 		if (!ft_isdigit(str[i]))
-		{
-			ft_dprintf(2, "minishell: exit: %s: numeric argument required\n",
-				str);
 			return (2);
-		}
 		i++;
 	}
 	return (0);
 }
 
+// Fonction pour gérer les erreurs d'argument non numérique
+static int	handle_numeric_argument_error(char *arg, t_data *data)
+{
+	printf("exit\n");
+	ft_dprintf(2, "minishell: exit: %s: numeric argument required\n", arg);
+	return (clear_exit(data, 2), 2);
+}
+
+// Fonction pour gérer le cas de trop d'arguments
+static int	handle_too_many_arguments(t_data *data)
+{
+	printf("exit\n");
+	ft_dprintf(2, "minishell: exit: too many arguments\n");
+	data->status = 127;
+	return (127);
+}
+
+// Fonction pour gérer la sortie avec un seul argument numérique
+static int	handle_exit_with_argument(char *arg, t_data *data)
+{
+	int	exit_code;
+
+	if (check_only_digit(arg))
+		return (handle_numeric_argument_error(arg, data));
+	exit_code = (unsigned char)ft_atoi(arg);
+	clear_exit(data, exit_code);
+	return (exit_code);
+}
+
+// Fonction principale
 int	ft_exit(char **tab, t_data *data)
 {
 	if (tab[1] && tab[2])
 	{
-		printf("exit\n");
-		perror("minishell: exit: too many arguments\n");
-		data->status = 127;
-		return (127);
+		if (check_only_digit(tab[1]))
+			return (handle_numeric_argument_error(tab[1], data));
+		return (handle_too_many_arguments(data));
 	}
-	else if (tab[1])
-	{
-		printf("exit\n");
-		data->status = check_only_digit(tab[1]);
-		data->status = (unsigned char)ft_atoi(tab[1]);
-		clear_exit(data, data->status);
-	}
-	else
-	{
-		printf("exit\n");
-		clear_exit(data, data->status);
-	}
+	if (tab[1])
+		return (handle_exit_with_argument(tab[1], data));
+	printf("exit\n");
+	clear_exit(data, data->status);
 	return (0);
 }
