cc4d2cdac98c1b482dd35bb12314377df5ac0aa1
Akhmed Dovletov <akdovlet@student.42.fr>
2024-12-21 15:12:19 +0100

    FORMAT: removed useless functions

--- include/builtins.h
+++ include/builtins.h
@@ -6,7 +6,7 @@
 /*   By: akdovlet <akdovlet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/26 12:34:29 by gschwand          #+#    #+#             */
-/*   Updated: 2024/12/21 11:37:40 by akdovlet         ###   ########.fr       */
+/*   Updated: 2024/12/21 15:07:56 by akdovlet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@
 int		builtin_echo(char **strs);
 int		builtin_cd(t_data *data, char **args, t_env **env);
 int		check_export(char *arg, t_data *data);
-int		go_home(t_env **env);
 int		ft_pwd(char **args, t_env *env);
 int		ft_env(char **args, t_env *env);
 int		ft_export(char **args, t_data *data);
@@ -31,6 +30,5 @@ int		unset(char **tab, t_data *data);
 int		ft_exit(char **tab, t_data *data);
 int		check_overflow(char *str, int n);
 char	*remove_quotes(char *str);
-char	*get_hard_path(char *path);
 
 #endif
